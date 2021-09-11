a9330eacf01caabc0f5eeb36fce144e105aa3c5a
louidaking123 <louidaking123@protonmail.com>
2021-09-11 07:20:09 +0300

    2 files modified

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ import './style.css';
 
 // Writing Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Start</h1>`;
+appDiv.innerHTML = `<h1>Hello Friend.</h1>`;
